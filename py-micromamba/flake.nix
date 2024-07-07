bf9bb4c3a3b00025ce319a8d55ea445f45cad3a4
Sidharth Arya <code@sidhartharya.com>
2024-07-07 13:26:41 +0530

    micromamba

---
+++ py-micromamba/flake.nix
@@ -0,0 +1,33 @@
+{
+description = "hello world";
+  inputs = {
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = {
+    systems,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    eachSystem = f:
+      nixpkgs.lib.genAttrs (import systems) (
+        system:
+          f nixpkgs.legacyPackages.${system}
+      );
+  in {
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.conda
+          pkgs.micromamba
+        ];
+        shellHook = ''
+            eval "$(micromamba shell hook --shell $SHELL)"
+            micromamba activate temp '';
+
+
+      };
+    });
+  };
+}
